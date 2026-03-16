d91be58a58a88d430b7a2ee56924284d21c84fbd
Kiran Jonathan <kiran.jonathan@stfc.ac.uk>
2026-03-17 00:31:26 +0100

    Fixed profiling example to give better timings, and to make runtime more reasonable

--- exercises/Unoptimised.py
+++ exercises/Unoptimised.py
@@ -6,7 +6,7 @@ def print_greeting():
     print("Hello ICSC26!")
     time.sleep(0.5)
     print("Welcome to the Simutron 5000")
-    print("We will now calculate your output")
+    print("We will now calculate your output, please wait...")
 
 
 def estimate_pi(n_samples=10_000_000):
@@ -20,23 +20,37 @@ def estimate_pi(n_samples=10_000_000):
     return np.round(pi, decimals=2)  # Removes randomness to make testing simpler
 
 
-def calculate_acceleration(theta_slope_pi_frac, g=9.81):
-    pi = estimate_pi()
-    return g * np.cos(theta_slope_pi_frac * pi)
+def calculate_acceleration(theta_slope, g=9.81):
+    return g * np.sin(theta_slope)
 
 
-def time_to_ground(theta_slope_pi_frac, slope_length, v_slope_0, dt=0.01):
+def time_to_ground(theta_slope, slope_length, v_slope_0, dt=1e-6):
     x_slope = 0
     v_slope = v_slope_0
 
     time = 0
     while x_slope < slope_length:
-        accel = calculate_acceleration(theta_slope_pi_frac)
+        accel = calculate_acceleration(theta_slope)
         v_slope = v_slope + accel * dt
         x_slope = x_slope + v_slope * dt
         time += dt
 
     return time
+
+
+def print_statistics(slope_length, slope_fracs_to_test, times):
+    for i in range(len(times)):
+        print(
+            f"Time for mass to travel down slop of length {slope_length}m and incline {slope_fracs_to_test[i] * estimate_pi()} rad: "
+        )
+        print(f"{times[i]}s")
+
+    sum = 0
+    for i in range(len(times)):
+        sum += times[i]
+    mean_time = sum / len(times)
+
+    print(f"Mean time to ground across all runs: {mean_time}s")
 
 
 def print_closing():
@@ -47,34 +61,26 @@ def print_closing():
     print("We hope you'll recommend Simutron 5000 to your friends!")
 
 
-def print_statistics(slope_length, slope_fracs_to_test, times):
-    for i in range(len(times)):
-        print(
-            f"Time for mass to travel down slop of length {slope_length}m and incline {slope_fracs_to_test[i]}pi rad: "
-        )
-        print(f"{times[i]} s")
-
-    sum = 0
-    for i in range(len(times)):
-        sum += times[i]
-    mean_time = sum / len(times)
-    print(f"Mean time to ground across all runs: {mean_time}")
-
-
 def main():
     print_greeting()
 
     slope_length = 10
-    slope_fracs_to_test = [1 / 6, 1 / 3, 1 / 2, 2 / 3]
+    slope_fracs_to_test = [1 / 6, 1 / 3, 1 / 2]
 
     times = []
     for slope_angle_pi_frac in slope_fracs_to_test:
+        slope_angle = estimate_pi() * slope_angle_pi_frac
+
+        print(f"Calculating for slope angle = {slope_angle}rad...")
+
         time = time_to_ground(
-            theta_slope_pi_frac=slope_angle_pi_frac,
+            theta_slope=slope_angle,
             slope_length=slope_length,
             v_slope_0=0,
         )
         times.append(time)
+
+    print("Calculations finished!")
 
     print_statistics(slope_length, slope_fracs_to_test, times)
 
